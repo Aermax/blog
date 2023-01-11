3e902009f91ad6685df92e5769a5624665b92547
Sid Shelke <ausv2027@gmail.com>
2023-01-11 20:10:14 +0530

    Final Deployment

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const _ = require("lodash")
+const dotenv = require("dotenv")
+
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', false)
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,14 +18,40 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
-let posts = []
+
+
+
+mongoose.connect(process.env.MONGOPASS)
+
+
+const blogSchema = {
+  title:String,
+  content:String
+}
+
+const Blog = mongoose.model('Blog',blogSchema)
+
+
 
 
 app.get("/",(req,res)=>{
-  res.render('home',{
-    startingContent:homeStartingContent,
-    posts:posts
+  Blog.find({},function(err, foundItems){
+    if(!err){
+      if(foundItems){
+        res.render('home',{
+          startingContent:homeStartingContent,
+          posts:foundItems 
+        })
+      }else{
+        res.render('home',{
+          startingContent:homeStartingContent,
+          posts:null
+        })
+      }
+      
+    }
   })
+  
   
 })
 
@@ -38,29 +68,40 @@ app.get("/compose",(req,res)=>{
   res.render('compose')
 })
 
-app.get("/posts/:postName",(req,res)=>{
-  const requestTitle = _.lowerCase(req.params.postName)
-  posts.forEach((post)=>{
-    const storedTitle = _.lowerCase(post.title)
-    if(storedTitle === requestTitle){
-      res.render('post',{
-        title:post.title,
-        content:post.content
-      })
+app.get("/posts/:postId",(req,res)=>{
+  const requestId = req.params.postId
+  Blog.findOne({_id:requestId},function(err, post){
+    res.render("post", {
 
-    }
-    
+      title: post.title,
+      content: post.content
+ 
+    })
   })
-  
 })
+    
+  
+  
+
 
 app.post("/compose",(req,res)=>{
-  const post = {
+  const post =  new Blog({
     title: req.body.postTitle,
     content : req.body.postBody
-  }
-  posts.push(post)
-  res.redirect("/")
+  })
+
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  })
+  
+  
+
 
 })
 
